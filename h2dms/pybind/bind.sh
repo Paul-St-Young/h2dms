202954a623c80d4140ee509f5561e474c9519df4
Paul Young <yyang173@illinois.edu>
2019-04-21 14:21:32 -0500

    bind h2ham

--- h2dms/pybind/bind.sh
+++ h2dms/pybind/bind.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-files="h2hp.cpp h2hp.h common.h h2wf.h"
+files="h2hp.cpp h2hp.h common.h h2wf.h h2ham.h h2ham.cpp"
 for file in $files; do
   ln -s ../purecpp/$file .
 done
--- h2dms/pybind/pybind.cpp
+++ h2dms/pybind/pybind.cpp
@@ -3,13 +3,23 @@
 namespace py=pybind11;
 
 #include "h2hp.h"
+#include "h2ham.h"
 
 PYBIND11_MODULE(h2hp, m)
 {
   py::class_<H2HartreeProduct>(m, "H2HartreeProduct")
     .def(py::init<double, double>())
+    .def_readonly("ions", &H2HartreeProduct::ions)
     .def("lnwf", &H2HartreeProduct::lnwf)
     .def("grad_lnwf", &H2HartreeProduct::grad_lnwf)
     .def("lap_lnwf", &H2HartreeProduct::lap_lnwf)
+    ;
+  py::class_<H2Hamiltonian>(m, "H2Hamiltonian")
+    .def(py::init<const Matrix&, const H2HartreeProduct&>())
+    .def("kinetic", &H2Hamiltonian::kinetic)
+    .def("ii", &H2Hamiltonian::ii)
+    .def("ei", &H2Hamiltonian::ei)
+    .def("ee", &H2Hamiltonian::ee)
+    .def("potential", &H2Hamiltonian::potential)
     ;
 }
