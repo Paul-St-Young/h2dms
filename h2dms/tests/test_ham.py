c7a145713b821e66cd1cc7256a54b67ebac3b05b
Paul Young <yyang173@illinois.edu>
2019-04-21 14:21:46 -0500

    test h2ham

---
+++ h2dms/tests/test_ham.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import numpy as np
+
+import sys
+sys.path.insert(0, '..')
+from pybind.h2hp import H2HartreeProduct, H2Hamiltonian
+
+# test system
+hp = H2HartreeProduct(1.4, 1.0)
+pos = np.array([
+  [-0.5, 0, 0],
+  [ 0.5, 0, 0]
+])
+dx = 1e-3
+
+def test_kinetic():
+  ham = H2Hamiltonian(hp.ions, hp)
+  assert np.isclose(9, ham.kinetic(pos))
+
+def test_potential():
+  ham = H2Hamiltonian(hp.ions, hp)
+  ii = 1./1.4
+  ei = -2*(1./0.2+1./1.2)
+  ee = 1.0
+  assert np.isclose(ii, ham.ii(pos))
+  assert np.isclose(ei, ham.ei(pos))
+  assert np.isclose(ee, ham.ee(pos))
+  pot = ii+ei+ei
+  assert np.isclose(ii+ei+ee, ham.potential(pos))
+
+if __name__ == '__main__':
+  test_kinetic()
+  test_potential()
+# end __main__
--- h2dms/tests/test_wf.py
+++ h2dms/tests/test_wf.py
@@ -45,3 +45,4 @@ def test_lap(atol=1e-4):
 
 if __name__ == '__main__':
   test_lap()
+# end __main__
